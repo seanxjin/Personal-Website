3ddb747a872e83e99f073f146b4c72a34845f5b2
Sean Jin <seanjin660@gmail.com>
2025-08-06 00:30:55 -0700

    Made Stars on the front page.

--- client/src/components/Star_texture.jsx
+++ client/src/components/Star_texture.jsx
@@ -1,75 +1,37 @@
 import "./Star_texture.css";
-import { useEffect, useRef } from "react";
 
 export default function Star_texture() {
 
+    let colors = ["#DEF6FF", "#A28AA6", "#9791BF", "#7C6EA3", "#193567", "#304B80", "#E5D7FF", "#CBECFF", "#BFE7FF", "#D0F3FF"];
 
-    const ref = useRef();
+    let stars = [];
 
-    useEffect(() => {
-        const canvas = ref.current;
-        const context = canvas.getContext("2d");
-        canvas.height = window.innerHeight;
-        canvas.width = window.innerWidth;
-
-        window.addEventListener("resize", () => {
-            canvas.height = window.innerHeight;
-            canvas.width = window.innerWidth;
-        });
-
-
-        const stars = [];
-
-        for (let i = 0; i < 400; i++) {
+    function createStars() {
+        for (let i=0; i<400; i++) {
+            const chosenColor = colors[Math.floor(Math.random() * colors.length)];
             let star = {
-                x: 0,
-                y: 0,
-                progress: Math.random(),
-                centerX: canvas.width / 2,
-                centerY: canvas.height,
-                radius: Math.random() * 1.5 + 0.5,
-                pathRadius: 250 + Math.random() * (canvas.width - 250),
-                speed: Math.random() * 0.0002 + 0.0001
+                position: "absolute",
+                top: ((Math.random() * 97) + 2).toString() + "%",
+                left: ((Math.random() * 97) + 2).toString() + "%",
+                width: ((Math.random() * 0.1) + 0.05).toString() + "rem",
+                height: ((Math.random() * 0.1) + 0.05).toString() + "rem",
+                background: chosenColor,
+                borderRadius: "50%",
+                filter: "brightness(3)"
             };
             stars.push(star);
         }
+    }
 
-
-        function animate() {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.fillStyle = `transparent`;
-            context.fillRect(0, 0, canvas.width, canvas.height);
-
-            for (let star of stars) {
-                star.progress += star.speed;
-
-                if (star.progress >= 1) {
-                    star.progress = 0;
-                }
-
-                const t = Math.PI * (1 - star.progress);
-
-                star.x = star.centerX + star.pathRadius * Math.cos(t);
-                star.y = star.centerY + star.pathRadius * -Math.sin(t);
-
-                context.beginPath();
-                context.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
-                context.fillStyle = `white`;
-                context.fill();
-            }
-
-            requestAnimationFrame(animate);
-        }
-
-        animate();
-
-
-    }, []);
-
+    createStars();
 
     return (
-        <canvas ref={ref} id="starry-canvas-container">
-
-        </canvas>
+        <div id="starry-canvas-container">
+            {stars.map((star, index) => {
+                return ( 
+                <div style={star} key={index}/>
+                );
+            })}
+        </div>
     );
 }
