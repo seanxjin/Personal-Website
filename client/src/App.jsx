9d9f6c6b1fae1ddb54bdf6c012395da784c630d6
Sean Jin <seanjin660@gmail.com>
2025-08-18 21:59:34 -0700

    Updated personal website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Star_texture from './components/Star_texture';
+import {motion} from "motion/react";
+import {useState, useEffect} from "react";
 
 function App() {
+
+    
+
+
+    useEffect(() => {
+
+    })
 
     return (
         <>
             <div id='hero-page-container'>
                 <Star_texture />
                 <Navbar />
+                <div id='title-name-container'>
+                    <h1>
+                        Hello, I'm Sean
+                    </h1>
+                    <motion.span>
+                        An aspiring{" "}
+                    </motion.span>
+                </div>
             </div>
+            
         </>
 
     );
--- client/src/components/Star_texture.jsx
+++ client/src/components/Star_texture.jsx
@@ -1,13 +1,19 @@
 import "./Star_texture.css";
+import { motion } from "motion/react"
+import { useState, useEffect } from "react"
 
 export default function Star_texture() {
 
-    let colors = ["#DEF6FF", "#A28AA6", "#9791BF", "#7C6EA3", "#193567", "#304B80", "#E5D7FF", "#CBECFF", "#BFE7FF", "#D0F3FF"];
+    const [stars, setStars] = useState([]);
+    const [meteors, setMeteors] = useState([]);
 
-    let stars = [];
+    useEffect(() => {
 
-    function createStars() {
-        for (let i=0; i<400; i++) {
+        const colors = ["#DEF6FF", "#A28AA6", "#9791BF", "#7C6EA3", "#193567", "#304B80", "#E5D7FF", "#CBECFF", "#BFE7FF", "#D0F3FF"];
+
+        const generatedStars = [];
+
+        for (let i = 0; i < 400; i++) {
             const chosenColor = colors[Math.floor(Math.random() * colors.length)];
             let star = {
                 position: "absolute",
@@ -17,19 +23,84 @@ export default function Star_texture() {
                 height: ((Math.random() * 0.1) + 0.05).toString() + "rem",
                 background: chosenColor,
                 borderRadius: "50%",
-                filter: "brightness(3)"
+                filter: "brightness(3) contrast(4) blur(0.02rem)"
             };
-            stars.push(star);
+            generatedStars.push(star);
         }
-    }
+        setStars(generatedStars);
+        const generatedMeteors = [];
+        for (let i = 0; i < 10; i++) {
+            let meteor = {
+                position: "absolute",
+                top: "-1rem",
+                left: ((Math.random() * 75) + 20).toString() + "%",
+                background: "white",
+                filter: "blur(0.5px) brightness(3) contrast(500%)",
+                borderRadius: "50%",
+                width: "0.3rem",
+                height: "0.3rem",
+                tail: {
+                    position: "absolute",
+                    top: "10%",
+                    width: 0,
+                    height: "2px",
+                    background: "linear-gradient(90deg, #ffffff, transparent)",
+                }
+            }
+            generatedMeteors.push(meteor);
+            setMeteors(generatedMeteors);
+        }
 
-    createStars();
+
+    }, []);
 
     return (
         <div id="starry-canvas-container">
             {stars.map((star, index) => {
-                return ( 
-                <div style={star} key={index}/>
+                return (
+                    <div style={star} key={index} />
+                );
+            })}
+            {meteors.map((meteor, index) => {
+                const { tail, ...meteorStyles } = meteor;
+                const sharedDelay = Math.random() * 30 + 5;
+                const sharedRepeatDelay = Math.random() * 30 + 5;
+                return (
+                    <motion.div
+                        key={index}
+                        style={{ ...meteorStyles }}
+                        initial={{
+                            rotate: -40,
+                            x: "0rem",
+                            y: "0rem",
+                            scale: 2,
+                            opacity: 1
+                        }}
+                        animate={{
+                            x: "-25rem",
+                            y: "35rem",
+                            scale: 0,
+                            opacity: 0,
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeOut",
+                            repeat: Infinity,
+                            repeatDelay: sharedRepeatDelay,
+                            delay: sharedDelay
+                        }}>
+                        <motion.div style={tail} initial={{
+                            width: 0
+                        }} animate={{
+                            width: 200
+                        }} transition={{
+                            duration: 2,
+                            ease: "easeOut",
+                            repeat: Infinity,
+                            repeatDelay: sharedRepeatDelay,
+                            delay: sharedDelay
+                        }}></motion.div>
+                    </motion.div>
                 );
             })}
         </div>
